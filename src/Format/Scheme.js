607bf9f828c5898dbc6fab65e5edb5f6b83c0905
delatal <40421533+delatal@users.noreply.github.com>
2019-03-05 10:18:08 +0100

    complexTypeDefinition undefined

--- src/Format/Scheme.js
+++ src/Format/Scheme.js
@@ -16,35 +16,36 @@ L.Format.Scheme = L.Class.extend({
       geometryField: this.options.geometryField
     });
     var complexTypeDefinition = element.getElementsByTagNameNS(L.XmlUtil.namespaces.xsd, 'complexType')[0];
-    var properties = complexTypeDefinition.getElementsByTagNameNS(L.XmlUtil.namespaces.xsd, 'sequence')[0];
-    for (var i = 0; i < properties.children.length; i++) {
-      var node = properties.children[i];
-      var propertyAttr = node.attributes.name;
-      if (!propertyAttr) {
-        continue;
+    if (complexTypeDefinition) {
+      var properties = complexTypeDefinition.getElementsByTagNameNS(L.XmlUtil.namespaces.xsd, 'sequence')[0];
+      for (var i = 0; i < properties.children.length; i++) {
+        var node = properties.children[i];
+        var propertyAttr = node.attributes.name;
+        if (!propertyAttr) {
+          continue;
+        }
+
+        var propertyName = node.attributes.name.value;
+        var typeAttr = node.attributes.type;
+        if (!typeAttr) {
+          var restriction = node.getElementsByTagNameNS(L.XmlUtil.namespaces.xsd, 'restriction');
+          typeAttr = restriction.attributes.base;
+        }
+
+        if (!typeAttr) {
+          continue;
+        }
+
+        var typeName = typeAttr.value.split(':').pop();
+
+        if (propertyName === this.options.geometryField) {
+          featureType.geometryFields[propertyName] = typeName;
+          continue;
+        }
+
+        featureType.appendField(propertyName, typeName);
       }
-
-      var propertyName = node.attributes.name.value;
-      var typeAttr = node.attributes.type;
-      if (!typeAttr) {
-        var restriction = node.getElementsByTagNameNS(L.XmlUtil.namespaces.xsd, 'restriction');
-        typeAttr = restriction.attributes.base;
-      }
-
-      if (!typeAttr) {
-        continue;
-      }
-
-      var typeName = typeAttr.value.split(':').pop();
-
-      if (propertyName === this.options.geometryField) {
-        featureType.geometryFields[propertyName] = typeName;
-        continue;
-      }
-
-      featureType.appendField(propertyName, typeName);
     }
-
     return featureType;
   }
 });
